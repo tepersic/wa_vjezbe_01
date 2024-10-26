ffd4afec6e059699f711255ee74bedbf83eed7d5
JamesWood1234 <88801850+JamesWood1234@users.noreply.github.com>
2024-10-26 17:06:37 +0200

    Samostalni zadatak za vježbu 1

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+
 
 const PORT = 3000;
 app.get("/", function (req, res) {
-    res.send("Hello World!");
+    res.send("<h1>Hello Express</h1>");
+});
+app.get("/about", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/about.html'));
+});
+app.get('/users', function (req, res) {
+    res.json({ 
+   "employees":[
+  {id: "1", ime:"Mario", prezime :"Manic"},
+  {id: "2", ime:"Mladen", prezime :"Bakic"},
+  {id: "3", ime:"Alen", prezime :"Pakic"},
+  
+]
+    });
+ 
 });
 
 app.listen(PORT, (error) => {
